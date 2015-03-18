d23b139f819013b8a1f57bbe073bf2a1657d663a
carcheky <carcheky@gmail.com>
2015-03-18 10:01:07 +0100

    actualizado script

--- download-druparcheky.sh
+++ download-druparcheky.sh
@@ -1,4 +1,4 @@
 #!/bin/bash  
 source ~/.bash_profile      
-drush -y make ./profiles/druparcheky/druparcheky.make
-rm descargartodo.sh
+drush -y make profiles/druparcheky/druparcheky.make --concurrency=5 --force-complete --no-cache
+rm download-druparcheky.sh
